dbac056d525863293b8b22718b752e3379bead18
sudhakama <75236347+sudhakama@users.noreply.github.com>
2020-12-10 09:11:27 +0530

    Add files via upload

---
+++ list random.py
@@ -0,0 +1,11 @@
+import random
+temp=0
+while True:
+    list1=[]
+    for i in range(0,10):
+        x=random.randint(1,100)
+        y=list1.append(x)
+    print(list1)
+    temp=temp+1
+    if temp == 10:
+        break
